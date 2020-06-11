5c0c8c79aea6f94df830afa55be5f382664600d8
fzovpec <fzovpec2@gmail.com>
2020-06-11 14:34:06 +0200

    Loginning and corrected bug. Right now this thing gets just the last post and text from it. Still not ready, need to implement multi-post-scrapping behavior

--- marketing_analysis/instagram_parser.py
+++ marketing_analysis/instagram_parser.py
@@ -1,9 +1,11 @@
+import selenium
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import InvalidArgumentException
+import time
 
 
 class InstagramPageParser():
@@ -12,15 +14,35 @@ class InstagramPageParser():
         self.driver = driver(ChromeDriverManager().install(), chrome_options=options)
         self.url = url
 
+    def login(self, login='username', password='password'):
+        ''' Logining user '''
+        u_input = self.driver.find_element_by_css_selector('input[name="username"]')
+        u_input.send_keys(login)
+        p_input = self.driver.find_element_by_css_selector('input[name="password"]')
+        p_input.send_keys(password)
+
+        login_btn = self.driver.find_element_by_css_selector(".DhRcB")
+        login_btn.click()
+
     def __call__(self):
         self.driver.get(self.url)
-        posts_div = self.driver.find_elements_by_class_name('_bz0w')  # getting a post
+
+        login_div = self.driver.find_element_by_class_name('eLAPa')
+        self.driver.execute_script("arguments[0].click();", login_div)
+        self.driver.implicitly_wait(10)
+        self.login()
+        time.sleep(5)
+
+        self.driver.get(self.url)
+        posts_div = self.driver.find_elements_by_class_name('eLAPa')  # getting a post
+
         for post_div in posts_div:
-            post_div.click()  # opening it
+            self.driver.execute_script("arguments[0].click();", post_div)
+
             opened_post = self.driver.find_element_by_class_name('PdwC2')
-            post_text_block = opened_post.find_elements_by_class_name('C4VMK')
-            post_text = post_text_block.find_element_by_tag_name('span')
-            print(post_text)
+            post_text_block = opened_post.find_element_by_class_name('C4VMK')
+            post_text = post_text_block.find_elements_by_tag_name('span')[1]
+            print(post_text.text)
             break
 
 
