94691789518f46a73b50fb5a12a3f305ddfc2e00
fzovpec <fzovpec2@gmail.com>
2020-06-11 14:52:10 +0200

    Now gets the text from the multiple posts

--- marketing_analysis/instagram_parser.py
+++ marketing_analysis/instagram_parser.py
@@ -14,7 +14,7 @@ class InstagramPageParser():
         self.driver = driver(ChromeDriverManager().install(), chrome_options=options)
         self.url = url
 
-    def login(self, login='username', password='password'):
+    def login(self, login='user', password='pass'):
         ''' Logining user '''
         u_input = self.driver.find_element_by_css_selector('input[name="username"]')
         u_input.send_keys(login)
@@ -23,6 +23,13 @@ class InstagramPageParser():
 
         login_btn = self.driver.find_element_by_css_selector(".DhRcB")
         login_btn.click()
+
+    def get_the_text(self, post):
+        ''' The function gets the text from the !OPENED! post'''
+        post_text_block = post.find_element_by_class_name('C4VMK')
+        post_text = post_text_block.find_elements_by_tag_name('span')[1]
+
+        return post_text
 
     def __call__(self):
         self.driver.get(self.url)
@@ -37,13 +44,15 @@ class InstagramPageParser():
         posts_div = self.driver.find_elements_by_class_name('eLAPa')  # getting a post
 
         for post_div in posts_div:
+            # opening the post and getting the essential info out of it
             self.driver.execute_script("arguments[0].click();", post_div)
 
             opened_post = self.driver.find_element_by_class_name('PdwC2')
-            post_text_block = opened_post.find_element_by_class_name('C4VMK')
-            post_text = post_text_block.find_elements_by_tag_name('span')[1]
+            post_text = self.get_the_text(opened_post)
             print(post_text.text)
-            break
+            # closing the post
+            close_btn = self.driver.find_element_by_class_name('wpO6b')
+            self.driver.execute_script("arguments[0].click();", close_btn)
 
 
 page_parse = InstagramPageParser(url='https://www.instagram.com/instagram/')
